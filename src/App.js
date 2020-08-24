92a666d83bf84a339ca3260874c7fe40789363ce
SakifKhan98 <61116402+SakifKhan98@users.noreply.github.com>
2020-08-24 07:13:32 +0600

    Added Material Ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Button from "@material-ui/core/Button";
+import { FormControl, InputLabel, Input } from "@material-ui/core";
+import "./App.css";
+import Message from "./Message";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+  console.log(input);
+  console.log(messages);
+
+  const sendMessage = (event) => {
+    //all the logic to send a message goes here
+    event.preventDefault();
+    setMessages([...messages, input]);
+    setInput("");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to Messenger!</h1>
+      <form>
+        <FormControl>
+          <InputLabel>Enter a message...</InputLabel>
+          <Input
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+          />
+          <Button
+            disabled={!input}
+            variant="contained"
+            color="primary"
+            type="submit"
+            onClick={sendMessage}
+          >
+            Send Message
+          </Button>
+        </FormControl>
+      </form>
+
+      {/* messages themselves */}
+      {messages.map((message) => (
+        <Message text={message}></Message>
+      ))}
     </div>
   );
 }
